ed4682c7f145352f2c38c2836403d2fa5868cc55
anserahmed <anserahmed.777@gmail.com>
2024-08-20 17:22:58 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,8 +25,6 @@ function Navigation() {
             </h1>
           </div>
 
-       
-
           {/* Bada navbar (Upper navbar)  */}
           <div className="md:flex  text-sm font-bold items-center hidden justify-between gap-7">
             <Link to="/">
@@ -42,7 +40,7 @@ function Navigation() {
             </Link>
             <Link to="/contact">
               <h4 className=" text-blue-500 hover:text-orange-400 transition ease-in">
-               RESOURCES
+                RESOURCES
               </h4>
             </Link>
 
@@ -80,7 +78,7 @@ function Navigation() {
 
           <div
             ref={navDialogeRef}
-            className="transition pt-6  hidden ease-in sm-menu bg-white fixed z-20 px-4 py-3 h-screen w-screen right-0 top-0"
+            className="transition pt-6 hidden ease-in sm-menu bg-white fixed z-20 px-4 py-3 h-screen w-screen right-0 top-0"
           >
             <div className="flex font-bold justify-between items-center">
               <h1 className="text-orange-500 text-3xl px-6 ">R M K</h1>
@@ -101,6 +99,7 @@ function Navigation() {
               </button>
             </div>
 
+            {/* Links */}
             <div className="flex flex-col justify-between  gap-5">
               <div className="flex flex-col  justify-between min-h-[80vh]">
                 <div className="mt-10 text-md font-semibold flex flex-col w-full gap-2 px-0">
@@ -181,35 +180,73 @@ function Navigation() {
                   </div>
                 </div>
               </div>
-              <div className="flex relative flex-end font-bold gap-5  justify-center items-center h-[10vh] bottom-0 ">
+
+              {/* footer (contacts) */}
+
+              <div className="flex relative flex-end font-bold gap-2  justify-center items-center h-[10vh] bottom-0 ">
                 <div>
                   <h3>Connect with us :</h3>
                 </div>
                 <div className="flex justify-start gap-2 items-center">
                   <h3>
                     <svg
-                      className="h-5 w-5 text-red-500"
+                      class="h-5 w-5 text-blue-500"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
                     >
-                      <path d="M20 6L9 17l-5-5" />
+                      {" "}
+                      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
                     </svg>
                   </h3>
                   <h3>
                     <svg
-                      className="h-5 w-5 text-blue-600"
+                      class="h-5 w-5 text-sky-500"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
                     >
-                      <path d="M20 6L9 17l-5-5" />
+                      {" "}
+                      <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+                    </svg>
+                  </h3>
+                  <h3>
+                    <svg
+                      class="h-5 w-5 text-red-500"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      {" "}
+                      <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+                      <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+                    </svg>
+                  </h3>
+                  <h3>
+                    <svg
+                      class="h-5 w-5 text-neutral-600"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      {" "}
+                      <circle cx="18" cy="5" r="3" />{" "}
+                      <circle cx="6" cy="12" r="3" />{" "}
+                      <circle cx="18" cy="19" r="3" />{" "}
+                      <line x1="8.59" y1="13.51" x2="15.42" y2="17.49" />{" "}
+                      <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
                     </svg>
                   </h3>
                 </div>
