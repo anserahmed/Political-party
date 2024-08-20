1d4e0d7a0859052bc9cb0eb08d89b8ef90e92dcc
anserahmed <anserahmed.777@gmail.com>
2024-08-20 16:59:12 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import "remixicon/fonts/remixicon.css";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 function Navigation() {
-  let navDialoge = document.getElementById("nav-dialoge");
+  const navDialogeRef = useRef(null);
 
   function handleMenu() {
-    navDialoge.classList.toggle("hidden");
+    if (navDialogeRef.current) {
+      navDialogeRef.current.classList.toggle("hidden");
+    }
   }
 
   return (
@@ -23,22 +25,7 @@ function Navigation() {
             </h1>
           </div>
 
-          <button onClick={handleMenu}>
-            <svg
-              class="h-8 w-8 md:hidden  text-gray-900"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              {" "}
-              <line x1="3" y1="12" x2="21" y2="12" />{" "}
-              <line x1="3" y1="6" x2="21" y2="6" />{" "}
-              <line x1="3" y1="18" x2="21" y2="18" />
-            </svg>
-          </button>
+       
 
           {/* Bada navbar (Upper navbar)  */}
           <div className="md:flex  text-sm font-bold items-center hidden justify-between gap-7">
@@ -73,27 +60,42 @@ function Navigation() {
             </div>
           </div>
 
+          <button onClick={handleMenu}>
+            <svg
+              className="h-8 w-8   text-gray-900"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="3" y1="12" x2="21" y2="12" />
+              <line x1="3" y1="6" x2="21" y2="6" />
+              <line x1="3" y1="18" x2="21" y2="18" />
+            </svg>
+          </button>
+
           {/* Navbar Chota wala (Side View) */}
 
           <div
-            id="nav-dialoge"
-            className="md:hidden transition pt-6 hidden ease-in sm-menu bg-white fixed z-20 px-4 py-3 h-screen w-screen right-0 top-0"
+            ref={navDialogeRef}
+            className="transition pt-6  hidden ease-in sm-menu bg-white fixed z-20 px-4 py-3 h-screen w-screen right-0 top-0"
           >
             <div className="flex font-bold justify-between items-center">
               <h1 className="text-orange-500 text-3xl px-6 ">R M K</h1>
 
               <button onClick={handleMenu}>
                 <svg
-                  class="h-8 w-8 text-gray-900"
+                  className="h-8 w-8 text-gray-900"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  {" "}
-                  <line x1="18" y1="6" x2="6" y2="18" />{" "}
+                  <line x1="18" y1="6" x2="6" y2="18" />
                   <line x1="6" y1="6" x2="18" y2="18" />
                 </svg>
               </button>
@@ -186,63 +188,28 @@ function Navigation() {
                 <div className="flex justify-start gap-2 items-center">
                   <h3>
                     <svg
-                      class="h-5 w-5 text-red-500"
+                      className="h-5 w-5 text-red-500"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
-                      {" "}
-                      <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
-                      <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+                      <path d="M20 6L9 17l-5-5" />
                     </svg>
                   </h3>
                   <h3>
                     <svg
-                      class="h-5 w-5 text-blue-500"
+                      className="h-5 w-5 text-blue-600"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
-                      {" "}
-                      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-                    </svg>
-                  </h3>
-                  <h3>
-                    <svg
-                      class="h-5 w-5 text-sky-500"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      {" "}
-                      <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-                    </svg>
-                  </h3>
-                  <h3>
-                    <svg
-                      class="h-5 w-5 text-neutral-600"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      {" "}
-                      <circle cx="18" cy="5" r="3" />{" "}
-                      <circle cx="6" cy="12" r="3" />{" "}
-                      <circle cx="18" cy="19" r="3" />{" "}
-                      <line x1="8.59" y1="13.51" x2="15.42" y2="17.49" />{" "}
-                      <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
+                      <path d="M20 6L9 17l-5-5" />
                     </svg>
                   </h3>
                 </div>
